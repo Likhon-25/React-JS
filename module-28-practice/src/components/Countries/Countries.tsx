91f7b35601387e2f1d5bbf08055a2bbea5cf1170
Likhon-25 <likhonmondol327@gmail.com>
2026-09-05 23:33:42 +0600

    Lift Up for flags and Handle Immutable array Practice

--- module-28-practice/src/components/Countries/Countries.tsx
+++ module-28-practice/src/components/Countries/Countries.tsx
@@ -1,4 +1,4 @@
-import { use } from "react";
+import { use, useState } from "react";
 import type { CountryType } from "../../type";
 import Country from "../Country/Country";
 
@@ -7,14 +7,26 @@ export interface CountriesProps {
 }
 
 export default function Countries({ countriesPromise }: CountriesProps) {
+  const [visitedCountries, setVisitedCountries] = useState<CountryType[]>([]);
+
   const countries = use(countriesPromise);
-  console.log(countries);
+
+  const handleVisitedCountry = (country: CountryType): void => {
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  };
+  // console.log(countries);
   return (
     <div>
       <h2>Countries: </h2>
+      <h4>Visited Countries: {visitedCountries.length}</h4>
       <div className="grid grid-cols-3">
         {countries.map((country) => (
-          <Country key={country.ccn3.ccn3} country={country}></Country>
+          <Country
+            key={country.ccn3.ccn3}
+            country={country}
+            handleVisitedCountry={handleVisitedCountry}
+          ></Country>
         ))}
       </div>
     </div>
--- module-28-practice/src/components/Country/Country.tsx
+++ module-28-practice/src/components/Country/Country.tsx
@@ -3,27 +3,32 @@ import type { CountryType } from "../../type";
 
 export interface CountryProps {
   country: CountryType;
+  handleVisitedCountry: (country: CountryType) => void;
 }
 
-export default function Country({ country }: CountryProps) {
-  const [visited, setVisited] = useState(false);
+export default function Country({ country, handleVisitedCountry }: CountryProps) {
+  const [visited, setVisited] = useState<boolean>(false); 
+
   const handleVisited = () => {
     setVisited(!visited);
+    handleVisitedCountry(country)
   };
   return (
-    <div className="border border-2 border-green-500  rounded-lg p-5 m-10">
+    <div className= {`border border-2 border-green-500  rounded-lg p-5 m-10 ${visited ? "bg-green-200" : ""} `}>
       <h3 className="font-bold text-3xl mb-5">{country.name.common}</h3>
       <img
         className="w-[200px]"
         src={country.flags.flags.png}
         alt={country.flags.flags.alt}
       />
+
       <button
         className=" border border-gray-500 mt-5 rounded-lg p-2"
         onClick={handleVisited}
       >
         {visited ? "Visited" : "Mark as Visited"}
       </button>
+      
     </div>
   );
 }
