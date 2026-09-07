e7598397064add147dacce34d327a55e6d8b7b7f
Likhon-25 <likhonmondol327@gmail.com>
2026-09-07 19:48:07 +0600

    Banner section completed

--- BPL-Dream-11/src/components/Nav.tsx
+++ BPL-Dream-11/src/components/Nav.tsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.png";
 const Nav = () => {
   return (
     <div>
-      <nav className="bg-gray-800 text-white flex items-center justify-between p-4 container mx-auto">
+      <nav className="bg-white text-black font-bold flex items-center justify-between p-4 container mx-auto border-2 border-gray-50">
         <img src={Logo} alt="BPL Dream 11 Logo" />
 
         <ul className="flex space-x-4 items-center">
